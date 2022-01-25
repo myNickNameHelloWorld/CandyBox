eaa0327ad5fc4495c7479a7a4c4b9b04a031e21a
nickolai <kolyabatman74@gmail.com>
2022-01-25 18:36:33 +0500

    created commit logger

--- src/main/java/ru/ibs/appline/framework/steps/Steps.java
+++ src/main/java/ru/ibs/appline/framework/steps/Steps.java
@@ -1,40 +1,49 @@
 package ru.ibs.appline.framework.steps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.ru.И;
 import ru.ibs.appline.framework.managers.PageManager;
 
 public class Steps {
+    private static final Logger logger = LoggerFactory.getLogger(Steps.class);
     PageManager pageManager = PageManager.getPageManager();
 
     @И("^Загружена стартовая страница$")
     public void getStartPage() {
         pageManager.getStartPage();
+        logger.info("Загружена стартовая страница");
     }
 
     @И("^Закрытие окна куки$")
     public void clickCloseCookies() {
         pageManager.getStartPage().clickCloseCookies();
+        logger.info("Закрытие окна куки");
     }
 
     @И("^Нажать на меню 'Ипотека'$")
     public void clickMortgageMenu() {
         pageManager.getStartPage().clickMortgageMenu();
+        logger.info("Нажать на меню 'Ипотека'");
     }
 
     @И("^Нажать на подменю 'Все ипотечные кредиты'$")
     public void clickAllMortgageProducts() {
         pageManager.getStartPage().clickAllMortgageProducts();
+        logger.info("Нажать на подменю 'Все ипотечные кредиты'");
     }
 
     @И("^Кликнуть на заголовок 'Готовое жилье'$")
     public void clickReadyHousing() {
         pageManager.getMortgagePage().clickReadyHousing();
+        logger.info("Кликнуть на заголовок 'Готовое жилье'");
     }
 
     @И("^Проверка заголовка открывшейся страницы$")
     public void checkTitle() {
         pageManager.getMortgageCalculatorPage().checkTitle();
+        logger.info("Проверка заголовка открывшейся страницы");
     }
 
     @И("^Заполнение формы поле/значение$")
@@ -42,17 +51,20 @@ public class Steps {
         dataTable.asMap(String.class, String.class)
                 .forEach((key, value) -> {
                     pageManager.getMortgageCalculatorPage().fillFieldPropertyValues((String) key, (String) value);
+                    logger.info("Заполняем форму: поле - '{}', значение - '{}'", key, value);
                 });
     }
 
     @И("^Кликнуть по кнопке 'Страхование жизни'$")
     public void clickCheckboxInsurance() {
         pageManager.getMortgageCalculatorPage().clickCheckboxInsurance();
+        logger.info("Проверка заголовка открывшейся страницы");
     }
 
     @И("^Проверка поля '(.*)'$")
     public void checkSum(String nameField) {
         pageManager.getMortgageCalculatorPage().checkFieldOnPage(nameField);
+        logger.info("Проверка поля '{}'", nameField);
     }
 
 
