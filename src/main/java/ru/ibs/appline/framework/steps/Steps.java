bf140741f5d6c13a010625527270a2198525b5f5
nickolai <kolyabatman74@gmail.com>
2022-01-25 13:18:34 +0500

    created steps commit

--- src/main/java/ru/ibs/appline/framework/steps/Steps.java
+++ src/main/java/ru/ibs/appline/framework/steps/Steps.java
@@ -4,8 +4,33 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.ru.И;
 import ru.ibs.appline.framework.managers.PageManager;
 
-public class MortgageCalculatorPageStep {
+public class Steps {
     PageManager pageManager = PageManager.getPageManager();
+
+    @И("^Загружена стартовая страница$")
+    public void getStartPage() {
+        pageManager.getStartPage();
+    }
+
+    @И("^Закрытие окна куки$")
+    public void clickCloseCookies() {
+        pageManager.getStartPage().clickCloseCookies();
+    }
+
+    @И("^Нажать на меню 'Ипотека'$")
+    public void clickMortgageMenu() {
+        pageManager.getStartPage().clickMortgageMenu();
+    }
+
+    @И("^Нажать на подменю 'Все ипотечные кредиты'$")
+    public void clickAllMortgageProducts() {
+        pageManager.getStartPage().clickAllMortgageProducts();
+    }
+
+    @И("^Кликнуть на заголовок 'Готовое жилье'$")
+    public void clickReadyHousing() {
+        pageManager.getMortgagePage().clickReadyHousing();
+    }
 
     @И("^Проверка заголовка открывшейся страницы$")
     public void checkTitle() {
@@ -29,4 +54,6 @@ public class MortgageCalculatorPageStep {
     public void checkSum(String nameField) {
         pageManager.getMortgageCalculatorPage().checkFieldOnPage(nameField);
     }
+
+
 }
